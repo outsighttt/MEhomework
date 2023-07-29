1a15c1f0b80663a63956b1607ce7daca2c42951e
outsighttt <sharifyanov.marat@mail.ru>
2023-07-29 17:15:57 +0300

    дз за 8 урок

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Advertisements
 
-# Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+    list_filter = ['auction', 'created_at']
+
+    actions = ['make_auction_as_false','make_auction_as_true']
+
+    fieldsets = (
+        ('Общее', {'fields': ('title', 'description')}),
+        ('Финансы', {'fields': ('price', 'auction'), 'classes': ['collapse']})
+    )
+
+    @admin.action(description='убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+    @admin.action(description='добавить возможность торга')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+admin.site.register(Advertisements, AdvertisementAdmin)
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
-# Create your models here.
 class Advertisements(models.Model):
     title = models.CharField(verbose_name='заголовок', max_length=128)
     description = models.TextField('описание объявления')
@@ -8,8 +9,26 @@ class Advertisements(models.Model):
     auction = models.BooleanField('возможность торга', help_text='Отметьте если торг уместен.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date()==timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html('<span style = "color:green; font-weight: bold"> Сегодня в {} <span/>', created_time)
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html('<span style = "color:green; font-weight: bold"> Сегодня в {} <span/>', updated_time)
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
     class Meta:
         db_table = 'advertisements'
+        verbose_name = 'Объявление'
 
     def __str__(self):
         return "%s object (%s)" % (self.__class__.__name__, f'id={self.id}, title={self.title}, price={float(self.price)}')
