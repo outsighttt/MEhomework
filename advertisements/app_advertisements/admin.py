721af88289676203de36d34d3ca601d03d3de075
outsighttt <sharifyanov.marat@mail.ru>
2023-08-09 22:26:27 +0300

    дз за 9 занятие

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from .models import Advertisements
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+    list_display = ['id', 'title', 'user', 'description', 'price', 'created_date', 'updated_date', 'auction',
+                    'show_image']
     list_filter = ['auction', 'created_at']
 
-    actions = ['make_auction_as_false','make_auction_as_true']
+    actions = ['make_auction_as_false', 'make_auction_as_true', 'set_default_image']
 
     fieldsets = (
-        ('Общее', {'fields': ('title', 'description')}),
+        ('Общее', {'fields': ('title', 'user', 'description', 'image')}),
         ('Финансы', {'fields': ('price', 'auction'), 'classes': ['collapse']})
     )
 
@@ -19,5 +20,9 @@ class AdvertisementAdmin(admin.ModelAdmin):
     @admin.action(description='добавить возможность торга')
     def make_auction_as_true(self, request, queryset):
         queryset.update(auction=True)
+
+    @admin.action(description='установить изображение по умолчанию')
+    def set_default_image(self, request, queryset):
+        queryset.update(image='adv.png')
 
 admin.site.register(Advertisements, AdvertisementAdmin)
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Advertisements(models.Model):
     title = models.CharField(verbose_name='заголовок', max_length=128)
@@ -9,22 +13,26 @@ class Advertisements(models.Model):
     auction = models.BooleanField('возможность торга', help_text='Отметьте если торг уместен.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    image=models.ImageField('изображение', upload_to='advertisements/', default='adv.png')
 
     @admin.display(description='дата создания')
     def created_date(self):
-        from django.utils import timezone
-        if self.created_at.date()==timezone.now().date():
+        if self.created_at.date() == timezone.now().date():
             created_time = self.created_at.time().strftime('%H:%M:%S')
             return format_html('<span style = "color:green; font-weight: bold"> Сегодня в {} <span/>', created_time)
         return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
 
     @admin.display(description='дата обновления')
     def updated_date(self):
-        from django.utils import timezone
         if self.updated_at.date() == timezone.now().date():
             updated_time = self.updated_at.time().strftime('%H:%M:%S')
             return format_html('<span style = "color:green; font-weight: bold"> Сегодня в {} <span/>', updated_time)
         return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='изображение')
+    def show_image(self):
+        return format_html('<img src=/media/{} height=50>', self.image)
 
     class Meta:
         db_table = 'advertisements'
